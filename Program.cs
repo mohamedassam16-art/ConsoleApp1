1d793e44bf8208cf9456fcbcb0d931f807ae75aa
mohamedassam16-art <mohamedassam162007@gmail.com>
2026-05-15 11:46:41 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// 1-what the whether (even/odd)
-/*
+
 Console.WriteLine("enter the whether");
 int number =
 int.Parse(Console.ReadLine());
@@ -11,10 +11,10 @@ else
 {
      Console.WriteLine("odd");
 }
-*/
+
 
 // 2-the absolute value
-/*
+
 
 Console.WriteLine("Enter The Value");
 int number = int.Parse(Console.ReadLine());
@@ -23,10 +23,10 @@ if(number <  0)
     number = number * -1;
 }
 Console.WriteLine("The absolute value is :" + number);
-*/
+
 
 // 3-find the larger
-/*
+
 Console.WriteLine("Enter The x");
 int x = int.Parse(Console.ReadLine());
 Console.WriteLine("Enter The y");
@@ -35,10 +35,10 @@ Console.WriteLine("Enter The z");
 int z = int.Parse(Console.ReadLine());
 int ruselt = Math.Max(x, Math.Max(y,z));
 Console.WriteLine("The Max Value" + ruselt);
-*/
+
 
 // 4-equel 2 x*3
-/*
+
 Console.WriteLine("Enter The a");
 int a = int.Parse(Console.ReadLine());
 Console.WriteLine("Enter The b");
@@ -49,7 +49,7 @@ if (a == b)
     sum = sum * 3; 
 }
 Console.WriteLine("The Value" + sum);
-*/
+
 
 // 5-true or false
 /*
